2c7e82c951255251dbb4ad64e07fc8393c50d29a
romelmahmud <romelmahmud@outlook.com>
2021-01-04 00:46:58 +0600

    animate fullscreen drop down

---
+++ app.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+
+   const burger = document.querySelector('.burger');
+   const nav = document.querySelector('ul');
+   const navLinks = document.querySelectorAll('ul li');
+
+
+   burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      navLinks.forEach((link, index) => {
+         if (link.style.animation) {
+            link.style.animation = '';
+         } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.6}s`
+
+         }
+         
+      });
+})
+
+}
+
+navSlide();
